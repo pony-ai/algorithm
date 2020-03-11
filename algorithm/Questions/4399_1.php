de518b4fd677d5cf7c96eb44d386cacd8922033d
ponyxiao <1663794971@qq.com>
2020-03-11 09:47:18 +0800

    update questions>>4399_1.php

--- algorithm/Questions/4399_1.php
+++ algorithm/Questions/4399_1.php
@@ -2,18 +2,14 @@
 /**
  * 一球从100米高度自由落下，每次落地后反跳回原高度的一半；再落下，求它在第n次落地时，共经过多少米？第n次反弹多高？（n<=10）
  */
-class calculate
-{
-    public $d=0;
-    public $h=100;
-    public function cal($n){
-        for($i=0;$i<$n;$i++){
-            $this->d += $this->h;
-            $this->h = $this->h/2;
-        }
-        return [$this->d,$this->h];
-    }
-}
 
-$test = new calculate();
-$test->cal(3);
+function cal($n){
+    $d=100;
+    $h=100;
+    for($i=1;$i<=$n;$i++){
+        $d += $h;
+        $h = $h/2;
+    }
+    return [$d,$h];
+}
+var_dump(cal(10));
